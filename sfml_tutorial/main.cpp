d258be9787e050126f0ff3029f293a4577f3484d
MakDaffi <63809925+MakDaffi@users.noreply.github.com>
2021-06-19 23:13:39 +0400

    bird is fly!

--- sfml_tutorial/main.cpp
+++ sfml_tutorial/main.cpp
@@ -1,9 +1,13 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include "windows.h"
+#include<iostream>
 
 using namespace sf;
+using namespace std;
 
 int main() {
-
+	Clock clock; //создаем переменную времени, т.о. привязка ко времени(а не мощности/загруженности процессора). 
     // Создаем окно
 	RenderWindow window(VideoMode(1200, 1080), "DOOM 3000");
 	Image birdimage; //создаем объект Image (изображение)
@@ -13,16 +17,22 @@ int main() {
 	Texture birdtexture;//создаем объект Texture (текстура)
 	birdtexture.loadFromImage(birdimage);//передаем в него объект Image (изображения)
 
-	Sprite birdsprite;//создаем объект Sprite(спрайт)
+    Sprite birdsprite;//создаем объект Sprite(спрайт)
 	birdsprite.setTexture(birdtexture);//передаём в него объект Texture (текстуры)
-	birdsprite.setTextureRect(IntRect(0, 20, 275, 215));//получили нужный нам прямоугольник с котом
+	birdsprite.setTextureRect(IntRect(35, 40, 240, 195));//получили нужный нам прямоугольник
 	birdsprite.setPosition(100, 100);//задаем начальные координаты появления спрайта
 
+	float CurrentFrame = 0;//хранит текущий кадр
 	// Главный цикл приложения. Выполняется, пока открыто окно
 	while (window.isOpen())
 	{
+		float time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
+		clock.restart(); //перезагружает время
+		time = time / 800; //скорость игры
+
 		// Обрабатываем очередь событий в цикле
 		Event event;
+		
 		while (window.pollEvent(event))
 		{
 			// Пользователь нажал на «крестик» и хочет закрыть окно?
@@ -30,10 +40,37 @@ int main() {
 				// тогда закрываем его
 				window.close();
 		}
-		if (Keyboard::isKeyPressed(Keyboard::W)) birdsprite.move(0, -0.1); 
-		if (Keyboard::isKeyPressed(Keyboard::S)) birdsprite.move(0, 0.1);
-		if (Keyboard::isKeyPressed(Keyboard::D)) birdsprite.move(0.1, 0);
-		if (Keyboard::isKeyPressed(Keyboard::A)) birdsprite.move(-0.1, 0);
+
+		// Кнопочки
+		if (Keyboard::isKeyPressed(Keyboard::D)) { //если нажата клавиша стрелка влево или англ буква А
+			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+			if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+			birdsprite.setTextureRect(IntRect(35 + 275 * int(CurrentFrame), 40, 240, 195)); 
+			birdsprite.move(0.1 * time, 0);//происходит само движение персонажа влево
+		}
+		if (Keyboard::isKeyPressed(Keyboard::W)) {
+			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+			if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+			birdsprite.setTextureRect(IntRect(35 + 275 * int(CurrentFrame), 290, 240, 195));
+			birdsprite.move(0, -0.2 * time);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::S)) {
+			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+			if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+			birdsprite.setTextureRect(IntRect(35 + 275 * int(CurrentFrame), 540, 240, 195));
+			birdsprite.move(0, 0.05 * time);
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::A)) {
+			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+			if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+			birdsprite.setTextureRect(IntRect(35 + 275 * int(CurrentFrame), 40, 240, 195));
+			birdsprite.move(-0.1 * time, 0);
+		}
+		
+	    // Падение птицы
+		birdsprite.move(0, 0.1 * time);
+
 		// Задаем цвет фона
 		window.clear(Color(0, 255, 255));
 		// Отрисовка круга
